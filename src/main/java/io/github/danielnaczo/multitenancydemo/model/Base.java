7e4c353d4a92234cb6778cdcae5c8c5fa037a33a
Daniel Naczinski <naczinski.daniel@gmail.com>
2023-04-03 17:07:23 +0200

    Modified auditing to save timestamps

--- src/main/java/io/github/danielnaczo/multitenancydemo/model/Base.java
+++ src/main/java/io/github/danielnaczo/multitenancydemo/model/Base.java
@@ -1,13 +1,11 @@
 package io.github.danielnaczo.multitenancydemo.model;
 
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.MappedSuperclass;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.io.Serializable;
 import java.time.LocalDateTime;
@@ -15,6 +13,7 @@ import java.time.LocalDateTime;
 @MappedSuperclass
 @Getter
 @Setter
+@EntityListeners(AuditingEntityListener.class)
 public abstract class Base implements Serializable {
 
     @Id
