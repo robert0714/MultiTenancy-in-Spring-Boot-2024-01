a68bceb1188a1010c743b08433229fb55c0c0384
robert0714 <robert0714@gmail.com>
2024-06-19 21:54:21 +0800

    adjust to learn it

---
+++ initdb.sql
@@ -0,0 +1,17 @@
+create database shared
+    with
+    owner = postgres
+    encoding = 'utf8'
+    template = template0;
+
+create database germany
+    with
+    owner = postgres
+    encoding = 'utf8'
+    template = template0;
+
+create database spain
+    with
+    owner = postgres
+    encoding = 'utf8'
+    template = template0;    
--- src/main/java/io/github/danielnaczo/multitenancydemo/database/multitenancy/MultiTenancyKeys.java
+++ src/main/java/io/github/danielnaczo/multitenancydemo/database/multitenancy/MultiTenancyKeys.java
@@ -4,8 +4,11 @@ import static org.springframework.boot.jdbc.DatabaseDriver.POSTGRESQL;
 
 public class MultiTenancyKeys {
     public static final String DEFAULT_DB_NAME = "shared";
-    public static final String URL_PREFIX = "jdbc:postgresql://localhost:5432/";
+    // public static final String DEFAULT_DB_NAME = "germany";
+    // public static final String DEFAULT_DB_NAME = "spain";
+    
+    public static final String URL_PREFIX = "jdbc:postgresql://172.18.204.152:5432/";
     public static final String USERNAME = "postgres";
-    public static final String PASSWORD = "";
+    public static final String PASSWORD = "password";
     public static final String DRIVER_CLASS_NAME = POSTGRESQL.getDriverClassName();
 }
